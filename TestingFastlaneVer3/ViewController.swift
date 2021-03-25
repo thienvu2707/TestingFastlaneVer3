efd584f70ec3a48f153c9c149e371fe4a08c2a83
Thien Vu Le <vule@gumiviet.com>
2021-03-25 09:47:47 +0700

    feat update new UI

--- TestingFastlaneVer3/ViewController.swift
+++ TestingFastlaneVer3/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func actionNextViewButton(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(identifier: "SecondViewController") as! SecondViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
 
